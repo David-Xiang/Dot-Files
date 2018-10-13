02f0143d3fe5d73376587ce9670becd13f856827
David Xiang <xiangdongwei@pku.edu.cn>
2018-10-13 14:39:55 +0800

    modified template.cpp

--- template.cpp
+++ template.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#define optimizer_io ios_base::sync_with_stdio(0);cin.tie(0);
+#define ioOptimizer ios_base::sync_with_stdio(0);cin.tie(0);
 using namespace std;
 int main(){
     optimizer_io
