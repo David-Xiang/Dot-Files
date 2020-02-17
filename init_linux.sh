62c8f9f63ce28d31103a99e7c4f1c8400f520b64
David Xiang <xiangdongwei@pku.edu.cn>
2020-02-17 12:16:51 +0800

    install oh-my-zsh from local

--- init_linux.sh
+++ init_linux.sh
@@ -1,6 +1,11 @@
+# 前置命令
+# sudo apt install git
 sudo apt update
+echo 'y' | sudo apt upgrade
+echo 'y' | sudo apt autoremove
+
 echo 'y' | sudo apt install vim git tmux zsh wget curl
-echo 'n' | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo 'n' | sh install-zsh.sh # oh-my-zsh 21.Nov 2019
 sudo chsh -s /usr/bin/zsh
 git clone https://github.com/David-Xiang/Dot-Files.git
 cd Dot-Files
@@ -8,3 +13,6 @@ cp tmux.conf ~/.tmux.conf
 cp vimrc ~/.vimrc
 cp zshrc ~/.zshrc
 zsh
+
+# install pip3
+sudo apt install python3-pip
